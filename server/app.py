770c795f26e47d746117dba6b8f0748f52889e10
Bill Brown <bill.a.brown90@gmail.com>
2023-04-30 15:02:16 -0700

    resful api lab

--- server/app.py
+++ server/app.py
@@ -17,10 +17,39 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        all_plants = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(jsonify(all_plants), 200)
+        return response
+    def post(self):
+
+        form_data = request.get_json()
+
+        new_plant = Plant(
+            name=form_data['name'],
+            image=form_data['image'],
+            price=form_data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        plant_dict = new_plant.to_dict()
+        response = make_response(plant_dict, 201)
+        
+        return response
+    
+
+api.add_resource(Plants, '/plants')        
+        
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+        response = make_response(plant_dict, 200)
+        return response 
+
+api.add_resource(PlantByID, '/plants/<int:id>')  
         
 
 if __name__ == '__main__':
